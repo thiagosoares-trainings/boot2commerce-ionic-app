2e58ce0a72458e3040d53fe96cedab51476db607
thiago soares <thiago.soares.jr@gmail.com>
2018-06-03 18:08:33 -0300

    Capturando imagem com camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,3 +1,4 @@
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { API_CONFIG } from './../../config/api.config';
 import { ClienteService } from './../../services/cliente.service';
 import { ClienteDto } from './../../models/cliente.dto';
@@ -13,11 +14,14 @@ import { StorageService } from '../../services/storage.service';
 export class ProfilePage {
 
   cliente: ClienteDto;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               public storage: StorageService,
-              public clienteService: ClienteService
+              public clienteService: ClienteService,
+              private camera: Camera
             ) {
   }
 
@@ -46,6 +50,29 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id).subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
     }, error => {});
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+
+    }, (err) => {
+     // Handle error
+    });
+
+
   }
 
 }
